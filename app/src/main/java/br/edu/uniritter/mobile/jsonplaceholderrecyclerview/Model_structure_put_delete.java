f53ece9f2af4e84bb7b2768f8b703033e7b9df80
Bruno Moraes <bruno.moraes@usabit.com.br>
2022-04-29 16:25:38 -0300

    commit unico

---
+++ app/src/main/java/br/edu/uniritter/mobile/jsonplaceholderrecyclerview/Model_structure_put_delete.java
@@ -0,0 +1,2 @@
+package br.edu.uniritter.mobile.jsonplaceholderrecyclerview;public class Model_structure_put_delete {
+}
---
+++ app/src/main/java/br/edu/uniritter/mobile/jsonplaceholderrecyclerview/PutActivity.java
@@ -0,0 +1,2 @@
+package br.edu.uniritter.mobile.jsonplaceholderrecyclerview;public class PutActivity {
+}
